7be4afc0cae1c4fd804a8a74792a2f1f357a6821
nirmalprojects1988 <nirmal.chakraborty34@gmail.com>
2023-11-13 16:43:13 +0530

    removed space

--- tests/uploadfileexample.spec.ts
+++ tests/uploadfileexample.spec.ts
@@ -6,7 +6,7 @@ test.describe('upload file examples',()=>{
 
     test('Upload a file ', async({page}) => {
         const filepath='tests/testdata/test-failed-1.png'
-       // const filepath='/Users/nirmal/Desktop/pwts/tests/testdata/test-failed-1.png'
+       //const filepath='/Users/nirmal/Desktop/pwts/tests/testdata/test-failed-1.png'
         await page.goto('https://the-internet.herokuapp.com/upload');
         page.on('filechooser',async(filechooser)=>{
             await filechooser.setFiles([filepath])
