f7811c483906b6f42a677261ab7029879a58a2ad
nirmalprojects1988 <nirmal.chakraborty34@gmail.com>
2023-11-18 13:48:02 +0530

    Deleted recorded scripts

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://letcode.in/frame');
-  
-  await page.frameLocator('iframe[name="firstFr"]').getByPlaceholder('Enter name').fill('nirmal');
-});
