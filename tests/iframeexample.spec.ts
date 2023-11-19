3e33f9ce0fbbc1d11ec831e60158117ed0fbf153
nirmalprojects1988 <nirmal.chakraborty34@gmail.com>
2023-11-19 10:54:52 +0530

    added code for handling frames and refactored few

--- tests/iframeexample.spec.ts
+++ tests/iframeexample.spec.ts
@@ -3,8 +3,36 @@ import{test,expect} from "@playwright/test"
 
 test('Enter value inside inframe', async({page}) => {
    await page.goto('https://letcode.in/frame')
-        const frame=page.frameLocator('iframe[name="firstFr"]')
+   const frame=await page.frameLocator('iframe[name="firstFr"]')
     //frame.getByPlaceholder('Enter name').fill('nirmal')
-    frame.getByPlaceholder('Enter name').pressSequentially('nirmal')
+   await frame.getByPlaceholder('Enter name').pressSequentially('nirmal')
    await expect(frame.getByPlaceholder('Enter name')).toHaveValue('nirmal') 
+
+});
+
+test('work with nested frame', async({page}) => {
+   await page.goto('https://letcode.in/frame')
+   const frame=await page.frame({url:'https://letcode.in/innerFrame'})
+   frame?.getByPlaceholder('Enter email').fill('nirmal@gmail.com')
+});
+test('interact with frame by frame method', async({page}) => {
+   await page.goto('https://letcode.in/frame')
+   const frame=await page.frame('firstFr')
+   frame?.getByPlaceholder('Enter name').pressSequentially('nirmal')
+});
+test('get count of all frames', async({page}) => {
+  await page.goto('https://letcode.in/frame')
+  const frames=await page.frame('firstFr')
+ console.log(`Total child frame count ${frames?.childFrames()?.length ??'Frame not found'}`);
+
+  
+//   const frame = await page.frame({ name: 'firstFr' });
+
+//   if (frame) {
+//     const childFrames = await frame.childFrames();
+//     console.log(`Total child frame count ${childFrames.length}`);
+//   } else {
+//     console.log('Frame with name "firstFr" not found');
+//   }
+ 
 });
--- tests/workwithtable.spec.ts
+++ tests/workwithtable.spec.ts
@@ -1,6 +1,35 @@
 //@ts-check
 import{test,expect} from "@playwright/test"
 
-test('', () => {
-    
+
+test('Get each cell data', async({page}) => {
+   await page.goto('https://the-internet.herokuapp.com/tables')
+   let rowcount= await page.locator('//table[@id="table1"]//tbody//tr').count()
+   console.log(`total row count ${rowcount}`);// total row count
+   for(let i=1;i<=rowcount;i++){
+        console.log("Last Name:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[1]').innerText());
+        console.log("First Name:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[2]').innerText());
+        console.log("Email:"+ await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[3]').innerText());
+        console.log("Due:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[4]').innerText());
+        console.log("Web Site:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[5]').innerText());
+        console.log("=========")
+   }
+   
+});
+
+test('Get data by condition', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/tables')
+   let rowcount= await page.locator('//table[@id="table1"]//tbody//tr').count()
+   for(let i=1;i<=rowcount;i++){
+    let due = await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[4]').innerText();
+    if(due==='$50.00'){
+        console.log("Last Name:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[1]').innerText());
+        console.log("First Name:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[2]').innerText());
+        console.log("Email:"+ await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[3]').innerText());
+        console.log("Due:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[4]').innerText());
+        console.log("Web Site:" + await page.locator('//table[@id="table1"]//tbody//tr['+i+']//td[5]').innerText());
+        console.log("=========")
+    }
+}
+
 });
